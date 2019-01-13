befea7cfcb91a5ea8db3fe281302b76bf2eca97d
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-13 09:15:08 +0100

    Add users controller; controller for register, login and secret routes

---
+++ src/controllers/usersController.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/user')
+const { jwtSecret } = require('../config')
+
+signToken = user => {
+  return jwt.sign({
+    iss: 'Authenticator',
+    sub: user.id,
+    iat: new Date().getTime(),
+    exp: new Date().setDate(new Date().getDate() + 1) // Current time + 1 day ahead
+  }, jwtSecret)
+}
+
+module.exports = {
+  register: async (req, res, next) => {
+    const { name, email, password } = req.value.body
+
+    const foundUser = await User.findOne({ email })
+
+    if (foundUser) {
+      return res.status(403).json({ errors: { email: 'Email already exists' } })
+    }
+
+    const newUser = new User({ name, email, password })
+
+    await newUser.save()
+
+    const token = await signToken(newUser)
+
+    return res.status(200).json({ token })
+  },
+
+  login: async (req, res, next) => {
+    const token = await signToken(req.user)
+
+    return res.status(200).json({ token })
+  },
+
+  secret: async (req, res, next) => {
+    return res.status(200).json({ secret: 'Secret resource' })
+  }
+}
