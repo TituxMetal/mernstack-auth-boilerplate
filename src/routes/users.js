b5431ccf83eea29579547fea025d1efbe781dac0
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-16 11:13:49 +0100

    Update users routes; add facebook auth route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
 const router = require('express-promise-router')()
 
 const UsersController = require('../controllers/usersController')
-const { jwtStrategy, localStrategy, googleStrategy, githubStrategy } = require('../passport')
+const { jwtStrategy, localStrategy, googleStrategy, githubStrategy, facebookStrategy } = require('../passport')
 const { validateRegister, validateLogin } = require('../validation')
 
 router.post('/register', validateRegister, UsersController.register)
 router.post('/login', validateLogin, localStrategy, UsersController.login)
 router.post('/auth/google', googleStrategy, UsersController.login)
 router.post('/auth/github', githubStrategy, UsersController.login)
+router.post('/auth/facebook', facebookStrategy, UsersController.login)
 router.get('/secret', jwtStrategy, UsersController.secret)
 
 module.exports = router
