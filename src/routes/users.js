c36972af90a2bee431f4f1435f5a3cad0bd69f6c
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-16 10:41:04 +0100

    Update users routes; add github auth route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,13 @@
 const router = require('express-promise-router')()
 
 const UsersController = require('../controllers/usersController')
-const { jwtStrategy, localStrategy, googleStrategy } = require('../passport')
+const { jwtStrategy, localStrategy, googleStrategy, githubStrategy } = require('../passport')
 const { validateRegister, validateLogin } = require('../validation')
 
 router.post('/register', validateRegister, UsersController.register)
 router.post('/login', validateLogin, localStrategy, UsersController.login)
 router.post('/auth/google', googleStrategy, UsersController.login)
+router.post('/auth/github', githubStrategy, UsersController.login)
 router.get('/secret', jwtStrategy, UsersController.secret)
 
 module.exports = router
