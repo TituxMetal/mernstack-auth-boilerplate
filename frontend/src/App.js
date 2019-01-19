71abf118d4d1b9cac52ed6ff1c50606b2923dd3b
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-19 07:40:41 +0100

    Update App component; add the Navbar component, pages routes and initialize the sidenav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Materialize from 'materialize-css/dist/js/materialize'
 
-import Home from './pages/Home'
+import { Home, Login, Register, Dashboard } from './pages/'
+import { Navbar } from './components/layout/'
 
 class App extends Component {
+  componentDidMount() {
+    const sidenavEl = document.querySelectorAll('.sidenav')
+    Materialize.Sidenav.init(sidenavEl)
+  }
+
   render() {
     return (
       <BrowserRouter>
         <>
+          <Navbar />
           <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
           </Switch>
         </>
       </BrowserRouter>
