6b1d939b2e7cd68c331e3a570dc5893b7532120a
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-19 07:41:46 +0100

    Update index.js file; import material-icons

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'material-icons/iconfont/material-icons.css'
 import 'materialize-css/dist/css/materialize.min.css'
 
 import App from './App'
