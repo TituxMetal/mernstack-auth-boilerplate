a7dc0f7a02cc15fb9de57cd346483e2903729a34
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-28 06:49:29 +0100

    Update LoginForm component; add Formik, loginSchema and remove Form styled component

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -1,15 +1,44 @@
 import React from 'react'
+import { withFormik, Form } from 'formik'
 
 import { AuthButton, InputField } from '../layout'
-import { Form, Title } from '../styled'
+import { Title } from '../styled'
+import { withContext } from '../../context'
+import { loginSchema } from '../../validationSchema/auth'
 
-const LoginForm = () => (
-  <Form method='post'>
-    <Title>Login Component</Title>
-    <InputField fieldType='email' fieldName='email' fieldLabel='Email' />
-    <InputField fieldType='password' fieldName='password' fieldLabel='Password' />
-    <AuthButton text='Login' />
-  </Form>
-)
+const LoginForm = ({ errors, touched }) => {
+  const fields = [
+    { name: 'email', type: 'email', label: 'Email' },
+    { name: 'password', type: 'password', label: 'Password' }
+  ]
+  return (
+    <Form>
+      <Title>Login Component</Title>
+      {fields.map(field => (
+        <InputField
+          key={field.name}
+          field={field}
+          errors={errors}
+          touched={touched}
+        />
+      ))}
+      <AuthButton text='Login' />
+    </Form>
+  )
+}
 
-export default LoginForm
+const FormikLogin = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      email: email || '',
+      password: password || ''
+    }
+  },
+  validationSchema: loginSchema,
+  handleSubmit: (values, { props: { submitHandler }, setSubmitting }) => {
+    setSubmitting(false)
+    submitHandler(values)
+  }
+})
+
+export default withContext(FormikLogin(LoginForm))
