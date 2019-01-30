20034b3152861c741e8f8a7716ba63c3b081b29a
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-30 08:14:25 +0100

    Add Messages styled component

---
+++ frontend/src/components/styled/Messages.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Messages = styled.div`
+  padding: .5rem;
+  margin-bottom: 1.5rem;
+  background-color: tomato;
+  color: darkred;
+  font-weight: bold;
+  p {
+    margin: 0;
+  }
+`
+
+export default Messages
