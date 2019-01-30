8e1bd54a67856ea1ebdca14db72fbb94d1c120de
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-30 08:28:11 +0100

    Update index.js file; add BrowserRouter and wrap App component in it; now App component has access to the router props

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import 'material-icons/iconfont/material-icons.css'
 import 'materialize-css/dist/css/materialize.min.css'
 
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
 
 serviceWorker.unregister()
